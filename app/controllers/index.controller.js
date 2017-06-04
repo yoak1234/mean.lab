fe526cd0d6ecf404fc2dbf57b9252bcef76b7e7b
Nititham <s5535512111@phuket.psu.ac.th>
2017-06-04 16:19:51 +0700

    add pug (view engine)
    >npm install pug --save
    -set views path
    -set view engine

--- app/controllers/index.controller.js
+++ app/controllers/index.controller.js
@@ -1,7 +1,4 @@
 exports.home = function (req, res) {
-    res.send("Wellcome to MEAN lab.")
-    //res.render('index', {
-    //    message: "Hello world",
-    //    author: "Nititham Limjaiphet"
-    //});
+    //res.send("Wellcome to MEAN lab.");
+    res.render('index/home');
 }
