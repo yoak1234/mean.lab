1cd51e02732d347191a4a90eb78846a24d3fbe03
Nititham <s5535512111@phuket.psu.ac.th>
2017-06-04 11:36:51 +0700

    add body-parser
    >npm install body-parser --save

--- config/express.js
+++ config/express.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 module.exports = function () {
     var app = express();
+
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+    app.use(bodyParser.json());
+
     require('../app/routes/index.routes')(app);
     return app;
 }
