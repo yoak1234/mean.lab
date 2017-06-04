08edf17b6f0ebb65f306942653c3ff9b73e01719
Nititham <s5535512111@phuket.psu.ac.th>
2017-06-04 11:53:52 +0700

    add compression (บีบอัด response)

--- config/express.js
+++ config/express.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var morgan = require('morgan');
+var compression = require('compression');
 
 module.exports = function () {
     var app = express();
 
     app.use(morgan('dev'));
+    app.use(compression());
 
     app.use(bodyParser.urlencoded({
         extended: true
